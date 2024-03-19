e633daa3b283db20edfe685720269306a57f6da7
Evgeniya Emelina <evg.emelina@gmail.com>
2024-03-19 10:21:01 +0100

    Исправлено решение 1 задачи (add -> toggle)

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -3,9 +3,9 @@ const button = document.querySelector('.dropdown__value');
 const list = document.querySelector('.dropdown__list');
 const elements = document.querySelectorAll('.dropdown__item');
 button.addEventListener('click', () => {
-  list.classList.add('dropdown__list_active'); });
+  list.classList.toggle('dropdown__list_active'); });
 elements.forEach(item => {
-  item.addEventListener('click', () => {
+  item.addEventListener('click', (event) => {
     event.preventDefault();
     list.classList.remove('dropdown__list_active'); 
     button.textContent = item.textContent;
