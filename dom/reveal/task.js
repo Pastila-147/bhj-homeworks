f61fe5b286491e64d80ef9ec1b48ea5051a0e69e
Evgeniya Emelina <evg.emelina@gmail.com>
2024-03-21 21:23:22 +0100

    убрала отладку к 1 задаче

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,7 +2,6 @@
 const reveals = document.querySelectorAll('.reveal');
 function isVisible (element) {
   const{top,bottom} = element.getBoundingClientRect();
-  console.log('bottom='+bottom+', top='+top+', innerHeight='+window.innerHeight)
   if (top > 0 && bottom < window.innerHeight) 
     {element.classList.add('reveal_active')} 
   else
